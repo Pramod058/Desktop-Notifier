99984af4a5facd65c301540ca82d01bc9feb2361
Pramod058 <aryalpramod05@gmail.com>
2022-03-29 20:25:21 +0545

    Added notify method with all its functionality.

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+#module import
 from plyer import notification
+from time import sleep
+
+while True:
+    notification.notify(
+        title = "Break",
+        message= "Hi Time to take break!!",
+        timeout = 10,
+        )
+    sleep(3000)
